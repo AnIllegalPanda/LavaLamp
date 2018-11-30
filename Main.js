71949914674bbb864a96cc778882853f1ba3588d
garcia5 <garcia5@rpi.edu>
2018-11-29 19:59:04 -0500

    Better force effects

--- Main.js
+++ Main.js
@@ -3,8 +3,8 @@ var canvas = document.getElementById("canvas"),
   ctx = canvas.getContext("2d"),
   tempCanvas = document.createElement("canvas"),
   tempCtx = tempCanvas.getContext("2d"),
-  width = 512,
-  height = 512,
+  width = 900,
+  height = 900,
   threshold = 210,
   colors = { r: 255, g: 0, b: 0 }, cycle = 0,
   points = [];
@@ -12,12 +12,12 @@ var canvas = document.getElementById("canvas"),
 canvas.width = tempCanvas.width = width;
 canvas.height = tempCanvas.height = height;
 
-for (var i = 0; i < 50; i++) {
+for (var i = 0; i < 20; i++) {
   var x = Math.random() * width,
-    y = Math.random() * height,
+    y = height - (Math.random() * (height/5)),
     vx = Math.random()/2 - 0.25, // btn -0.25, 0.25
-    vy = (Math.random() * 2) - 1, // btn -1, 1
-    size = Math.floor(Math.random() * 60) + 60;
+    vy = Math.random() - 0.5, // btn -0.5, 0.5
+    size = Math.floor(Math.random() * 300) + 40; // btn 40, 340
 
   points.push({ x: x, y: y, vx: vx, vy: vy, size: size });
 
@@ -31,12 +31,14 @@ function update() {
     point.x += point.vx;
     point.y += point.vy;
 
+    if( point.y < 0 ) point.y = 0;
+    if( point.y > height ) point.y = height;
     // higher y is farther down the page
-    var Fg = 0.0125;
-    // |Fh| without scalar is <(height/2)**2
-    // multiply by 40 to keep |Fh| < 0.025
-    var fhScalar = (height/2)**2 * 40;
-    var Fh = -(((point.y-(height/2))**2) / fhScalar);
+    var Fg = 0.001;
+    // |Fh| without scalar is <(height/2)**3
+    // multiply by 40 to keep |Fh| < 0.0125
+    var fhScalar = (height/2)**3 * 80 * (point.size/120);
+    var Fh = -(((point.y-(height/2))**3) / fhScalar);
 
     point.vy += Fg;
     point.vy += Fh;
@@ -48,9 +50,10 @@ function update() {
 
     if (point.y > height ||
         point.y < 0) {
-      point.vy = 0 - point.vy;
+      point.vy = 0;
     }
 
+    // TODO: try changing this into webGL and buffers and such
     tempCtx.beginPath();
     var grad = tempCtx.createRadialGradient(point.x, point.y, 1, point.x, point.y, point.size);
     grad.addColorStop(0, 'rgba(' + colors.r + ',' + colors.g + ',' + colors.b + ',1)');
@@ -59,12 +62,27 @@ function update() {
     tempCtx.arc(point.x, point.y, point.size, 0, Math.PI * 2);
     tempCtx.fill();
   }
+  computeColor();
   metabalize();
-  colorCycle();
   setTimeout(update, 10);
 }
 
-function colorCycle(){
+// compute color based on Fh being applied
+function computeColor(heat){
+
+  /*
+  var scaledForce = 50000 * heat;
+  // clamp values to < 255
+  if( Math.abs(scaledForce) > 255 ) {
+    scaledForce = 255 * (scaledForce/Math.abs(scaledForce));
+  }
+
+  var ratio = 2 * (scaledForce+255) / 510;
+  colors.b = ~~(Math.max(0, 255*(ratio - 1)));
+  colors.r = ~~(Math.max(0, 255*(1 - ratio)));
+  colors.g = 255 - colors.b - colors.r;
+  */
+
   cycle+=0.1;
   if(cycle>100){
     cycle = 0;
@@ -89,14 +107,5 @@ function metabalize() {
   }
   ctx.putImageData(imageData, 0, 0);
 }
-
-/*
-function forces(ball) {
-  var Fg = -0.5;
-  var Fh = 1/(ball.y ** 2);
-
-  ball.y += (Fg + Fh);
-		}
-*/
 
 update();
